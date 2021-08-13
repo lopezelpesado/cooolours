6cccef93bc5b66114c6c17742b668967ea6396d3
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-08-13 18:55:15 +0100

    fetch and set named colors

--- script.js
+++ script.js
@@ -22,6 +22,40 @@ function randomHSLCooolour() {
 	return `${randomNumber0To360()}, ${randomNumber0To100()}%, ${randomNumber0To100()}%`;
 }
 
+// create list of random colours
+
+function createColoursList() {
+	return {
+		color1: randomHSLCooolour(),
+		color2: randomHSLCooolour(),
+		color3: randomHSLCooolour(),
+		color4: randomHSLCooolour(),
+		color5: randomHSLCooolour(),
+	};
+}
+
+// color fetcher
+
+function fetchColor(randomColorHSL) {
+	return fetch(
+		`https://www.thecolorapi.com/id?hsl=${randomColorHSL.replaceAll(" ", "")}`
+	).then((response) => response.json());
+}
+
+// fetch the randomly generated colours and replace them in the color list with actual colors
+
+async function fetchReplaceAndSetColors(list) {
+	let i = 0;
+	for (const color in list) {
+		fetchedColor = await fetchColor(list[color]);
+		list[color] = fetchedColor;
+		cooolours[i].style.backgroundColor = list[color].name.closest_named_hex;
+		cooolours[i].querySelector("h2").textContent = list[color].name.value;
+		i++;
+	}
+	console.log(list);
+}
+
 // set cooolours with random colours
 
 function setCooolours() {
@@ -30,6 +64,10 @@ function setCooolours() {
 	});
 }
 
-setCooolours();
+// stuff to do on page load
 
-console.log(randomHSLCooolour());
+const colorList = createColoursList();
+
+console.log(colorList);
+
+fetchReplaceAndSetColors(colorList);
