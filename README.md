5c6b2d5a3a2733271208ac0ea81e1bf017132136
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-08-13 16:48:24 +0100

    colour buttons

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cooolours
+
+A random colour scheme generator heavily inspired by the brilliant [coolor.co](https://coolors.co/).
--- script.js
+++ script.js
@@ -6,30 +6,30 @@ const cooolours = Array.from(document.getElementsByClassName("cooolours"));
 
 // random number between 0 and 255
 
-function randomNumber0To360 () {
-    return Math.floor(Math.random() * 361)
+function randomNumber0To360() {
+	return Math.floor(Math.random() * 361);
 }
 
 // random number between 0 and 100
 
-function randomNumber0To100 () {
-    return Math.floor(Math.random() * 101)
+function randomNumber0To100() {
+	return Math.floor(Math.random() * 101);
 }
 
 // random colour generator
 
-function randomHSLCooolour () {
-    return `${randomNumber0To360()}, ${randomNumber0To100()}%, ${randomNumber0To100()}%`
+function randomHSLCooolour() {
+	return `${randomNumber0To360()}, ${randomNumber0To100()}%, ${randomNumber0To100()}%`;
 }
 
 // set cooolours with random colours
 
-function setCooolours () {
-    cooolours.forEach(e => {
-        e.style.backgroundColor = `hsl(${randomHSLCooolour()})`
-    })
+function setCooolours() {
+	cooolours.forEach((e) => {
+		e.style.backgroundColor = `hsl(${randomHSLCooolour()})`;
+	});
 }
 
-setCooolours ()
+setCooolours();
 
-console.log(randomHSLCooolour())
+console.log(randomHSLCooolour());
